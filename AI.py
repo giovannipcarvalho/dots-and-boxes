14bd6ccf0f202de483e9bb502ce10b77e2371561
Giovanni Paolo <giovanni1995@gmail.com>
2017-03-08 00:19:18 -0300

    Remove excess code

--- AI.py
+++ AI.py
@@ -1,97 +1,17 @@
 import sys
 from DotsAndBoxes import *
 
-class Node(object):
-    def __init__(self, game=None, score=0, max=True, a=-np.inf, b=np.inf):
-        self.game = game
-        self.score = score
-        self.max = max
-        self.a = a
-        self.b = b
-
 def evaluate(game, player):
     player_score = game.score[player == 1]
     opponent_score = game.score[player == -1]
     return player_score - opponent_score
-
-def min_play(game):
-    if game.is_over():
-        return evaluate(game, game.turn)
-    moves = game.get_available_moves()
-    best_score = np.inf
-    for move in moves:
-        g = DotsAndBoxes(game_obj=game)
-        g.play(move)
-        if g.turn == game.turn: # keeps playing
-            score = min_play(g) # minimize again
-        else:
-            score = max_play(g)
-        if score < best_score:
-            best_move = move
-            best_score = score
-    return best_score
-
-def max_play(game):
-    if game.is_over():
-        return evaluate(game, game.turn)
-    moves = game.get_available_moves()
-    best_score = -np.inf
-    for move in moves:
-        g = DotsAndBoxes(game_obj=game)
-        g.play(move)
-        if g.turn == game.turn: # keeps playing
-            score = max_play(g) # maximize again
-        else:
-            score = min_play(g)
-        if score > best_score:
-            best_move = move
-            best_score = score
-    return best_score
-
-def mini_max(game):
-    moves = game.get_available_moves()
-    best_move = moves[0]
-    best_score = -np.inf
-    for move in moves:
-        g = DotsAndBoxes(game_obj=game)
-        g.play(move)
-        if g.turn == game.turn: # keeps playing
-            score = max_play(g) # maximize again
-        else:
-            score = min_play(g)
-        if score > best_score:
-            best_move = move
-            best_score = score
-    return best_move
 
 def copy_play(game, move):
     g = DotsAndBoxes(game_obj=game)
     g.play(move)
     return g
 
-def alpha_beta(game, max_depth=4):
-    def calc_value(game, alpha, beta, depth, maximize):
-        if should_stop(game, depth):
-            return evaluate(game, game.turn)
-        v = -np.inf if maximize else np.inf
-        for move in game.get_available_moves():
-            g = copy_play(game, move)
-            scored = g.turn == game.turn
-            if scored:
-                v = max(v, calc_value(g, alpha, beta, depth+1, True)) if maximize else min(v, calc_value(g, alpha, beta, depth+1, False))
-            else:
-                v = max(v, calc_value(g, alpha, beta, depth+1, False)) if maximize else min(v, calc_value(g, alpha, beta, depth+1, True))
-            
-            if maximize:
-                alpha = max(alpha, v)
-            else:
-                beta = min(beta, v)
-            
-            if beta <= alpha:
-                break
-        
-        return v
-    
+def alpha_beta(game, max_depth=4):    
     def max_value(game, alpha, beta, depth):
         if should_stop(game, depth):
             return evaluate(game, game.turn)
@@ -127,8 +47,7 @@ def alpha_beta(game, max_depth=4):
     should_stop = lambda game, depth: depth >= max_depth or game.is_over()
     
     moves = game.get_available_moves()
-
-    scores = map(lambda m: calc_value(copy_play(game, m), -np.inf, np.inf, 0, True), moves)
+    scores = map(lambda m: max_value(copy_play(game, m), -np.inf, np.inf, 0), moves)
     
     return moves[np.argmax(scores)]
 
