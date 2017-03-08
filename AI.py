9eb3e59c457619996ee6d7f0e769a9de0c836e87
Giovanni Paolo <giovanni1995@gmail.com>
2017-03-08 01:38:38 -0300

    Add alternative alpha-beta implementation

--- AI.py
+++ AI.py
@@ -51,6 +51,35 @@ def alpha_beta(game, max_depth=4):
     
     return moves[np.argmax(scores)]
 
+def alpha_beta_search(game, depth, a, b, maximize):
+    if depth == 0 or game.is_over():
+        return evaluate(game, game.turn)
+    
+    if maximize:
+        v = -np.inf
+        for move in game.get_available_moves():
+            g = copy_play(game, move)
+            if g.turn == game.turn:
+                v = max(v, alpha_beta_search(g, depth - 1, a, b, True))
+            else:
+                v = max(v, alpha_beta_search(g, depth - 1, a, b, False))
+            a = max(a, v)
+            if b <= a:
+                break
+        return v
+    else:
+        v = np.inf
+        for move in game.get_available_moves():
+            g = copy_play(game, move)
+            if g.turn == game.turn:
+                v = min(v, alpha_beta_search(g, depth - 1, a, b, False))
+            else:
+                v = min(v, alpha_beta_search(g, depth - 1, a, b, True))
+            b = min(b, v)
+            if b <= a:
+                break
+        return v
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         next_player = 'B'
@@ -61,8 +90,12 @@ if __name__ == "__main__":
         board_string = sys.argv[2]
     
     # game = DotsAndBoxes(PLAYERS[next_player], board_string=board_string)
-    game = DotsAndBoxes(-1, 3, 3)
+    game = DotsAndBoxes(-1, 4, 4)
+    print alpha_beta_search(game, 8, -np.inf, np.inf, True)
+    # moves = game.get_available_moves()
+    # scores = map(lambda m: alpha_beta_search(copy_play(game, m), 5, -np.inf, np.inf, True), moves)
+
     # print game.board
 
     # print mini_max(game)
-    print alpha_beta(game, 0)
+    # print alpha_beta(game, 5)
